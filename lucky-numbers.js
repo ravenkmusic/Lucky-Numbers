338e9c4f869b5161ecf499b2fd874785201a7756
Raven Hendricks <ravenkbeats@gmail.com>
2024-06-13 11:48:08 -0400

    added twoSum function

--- lucky-numbers.js
+++ lucky-numbers.js
@@ -4,3 +4,19 @@
  * The function should interpret each array as a number and return the sum of those two numbers.
  */
 
+/**
+ * Calculates the sum of the two input arrays.
+ *
+ * @param {number[]} array1
+ * @param {number[]} array2
+ * @returns {number} sum of the two arrays
+ */
+export function twoSum(array1, array2) {
+    let firstArray = array1.join("");
+    console.log(firstArray);
+    let secondArray = array2.join("");
+    console.log(secondArray);
+    let sum = Number(firstArray) + Number(secondArray)
+    console.log(sum);
+    return sum;
+  }
