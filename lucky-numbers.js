496e9aaf6e3c62f65e803b3a6827669a28455176
Raven Hendricks <ravenkbeats@gmail.com>
2024-06-13 10:23:47 -0400

    added comments

---
+++ lucky-numbers.js
@@ -0,0 +1,6 @@
+/**
+ * Write a function twoSum that accepts two arrays as parameters. 
+ * Each array consists of one or more numbers between 0 and 9. 
+ * The function should interpret each array as a number and return the sum of those two numbers.
+ */
+
